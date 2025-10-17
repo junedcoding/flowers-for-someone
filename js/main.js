d7e095b3d49c7762de76021134f6455d391193fd
junedcoding <junzthepublic@gmail.com>
2025-10-17 08:55:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('HAMASAH Kaka LAKSANA').split('')
+    const titles = ('KLARIZA RIZQIA SARI🤍🌹🫰').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
